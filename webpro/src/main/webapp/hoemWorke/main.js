511b78ebaf79a944bae082d27081a10c62996478
windy9109 <windy9109@naver.com>
2022-03-10 09:11:15 +0900

    2022-03-10

---
+++ webpro/src/main/webapp/hoemWorke/main.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+ 
+       $(document).ready(function(){
+         $("#header").load("head.html")   
+      });
+      
+      $(document).ready(function(){
+          $("#left").load("aside.html")   
+       });
+       
+     $(document).ready(function(){
+          $("#right").load("asideR.html")   
+       });
